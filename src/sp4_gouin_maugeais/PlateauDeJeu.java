c991779e0a50e62823c2a858f067f35f25c42098
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-11-28 16:56:21 +0100

    Ajout de méthodes pour PlateauDeJeu

--- src/sp4_gouin_maugeais/PlateauDeJeu.java
+++ src/sp4_gouin_maugeais/PlateauDeJeu.java
@@ -13,14 +13,39 @@ public class PlateauDeJeu { // Création classe Plateau de jeu
     CelluleDeGrille [][] grille = new CelluleDeGrille[6][7]; // création de la grille 
     
     public PlateauDeJeu(){  // constructeur du plateau de jeu 
-        CelluleDeGrille cellule_grille = null;
         for(int i=0; i<6; i++){
-            for(int j=0; i<7; j++){
-                grille[i][j] = null; // on attribue des cases vides a toutes les cellules grilles
+            for(int j=0; j<7; j++){
+                grille[i][j]= null; // on attribue des cases vides a toutes les cellules grilles
             }
    
         }
     }
     
+    public int ajouterJetonDansColonne(Jeton jetemp, int col){
+        for(int i = 6; i>=0 ; i--){
+            if (grille[i][col]==null){
+                grille[i][col].affecterJeton(jetemp);
+                break;
+            }
+        return i;
+            
+        }
+        return -1; 
+    } 
+    
+    public boolean grilleRemplie(){
+        boolean cond = true;
+        for(int i=0; i<6; i++){
+            for(int j=0; j<7; j++){
+                if (grille[i][j]==null){
+                    cond=false;
+                }
+            }
+        }
+        
+        return cond;   
+        }
+
     
 }
+
