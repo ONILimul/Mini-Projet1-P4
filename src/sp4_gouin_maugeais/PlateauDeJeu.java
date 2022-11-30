36692ee81d7de86b0d85237e0aa08fb0c8069314
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-11-30 14:42:28 +0100

    Création méthode colonnes gagnantes

--- src/sp4_gouin_maugeais/PlateauDeJeu.java
+++ src/sp4_gouin_maugeais/PlateauDeJeu.java
@@ -22,17 +22,18 @@ public class PlateauDeJeu { // Création classe Plateau de jeu
     }
     
     public int ajouterJetonDansColonne(Jeton jetemp, int col){  //méthode pour ajouter un jeton dans une colonne 
+        int ret = -1 ;
         for(int i = 6; i>=0 ; i--){
             if (grille[i][col]==null){ // Dès qu'une cellule de la colonne est vide on peut y placer le jeton 
                 grille[i][col].affecterJeton(jetemp);  // Donc on affecte le jeton à la cellule
+                ret = i ;
                 break;    // Puis on sort de la boucle afin de ne pas ajouter le même jeton dans les cellules au dessus
             }
-        return i;  // on retourne la ligne où le jeton a été affecté
-            
-        }
-        return -1; // Si la colonne est pleine on retourne -1 signifiant que la colonne est pleine 
-    } 
-    
+        }    
+        return ret ; // On retourne la ligne à laquelle on a ajouté le jeton si la colonne est pleine on retourne - 1 signifiant qu'on ne peut pas ajouter de jeton dans cette colonne.
+    }    
+         
+        
     public boolean grilleRemplie(){  // Méthode qui vérifie si notre grille est totalement remplie ou non 
         boolean cond = true;  
         for(int i=0; i<7; i++){  // On vérifie toutes les cellules de notre 1ère ligne
@@ -41,37 +42,49 @@ public class PlateauDeJeu { // Création classe Plateau de jeu
             }
         
         }
-       return cond;   // on retourne si notre grille est remplie ou non 
+       return cond;   // on retourne si notre grille est remplie ou non  FALSE: il reste une place TRUE: le tableau est plein
     }
-    public void afficherGrilleSurConsole() {
+    public void afficherGrilleSurConsole() {  // Méthode qui permet d'afficher la grille de jeu sur la console
         System.out.println(grille.toString());
     }
 
-    public boolean presenceJeton(int x, int y){
+    public boolean presenceJeton(int x, int y){  // Méthode réutilisant la méthode de cellule pour l'appliquer à une cellule de la grille de jeu 
         return grille[x][y].presenceJeton();
     }
-    public String lireCouleurDuJeton(int x, int y){
+    public String lireCouleurDuJeton(int x, int y){ // Méthode réutilisant la méthode de cellule pour l'appliquer à une cellule de la grille de jeu
         return grille[x][y].lireCouleurDuJeton();
     }
     
-    public boolean ligneGagnantePourCouleur (String couleur) {
-        boolean temp = false;
+    public boolean ligneGagnantePourCouleur (String couleur) { // Cette (sous) méthode indique si une couleur que l'on donne est gagnante sur une ligne de notre plateau de jeu ou non 
+        boolean temp1 = false;
         for (int a = 0; a < 6 ;a++){
-            for (int i = 0; i < 3; i++){
-                if ( lireCouleurDuJeton(a,i) == couleur || lireCouleurDuJeton(a,i+1) == couleur || lireCouleurDuJeton(a, i+2) == couleur || lireCouleurDuJeton(a, i+3) == couleur) {
-                    temp = true ;
+            for (int i = 0; i <= 3; i++){
+                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a,i+1) == couleur & lireCouleurDuJeton(a, i+2) == couleur & lireCouleurDuJeton(a, i+3) == couleur) {
+                    temp1 = true ;
                     break;
                 }
             }   
         }
-        return temp;
+    return temp1; // Retourne si il y a une ligne gagnante : TRUE la couleur gagne FALSE la couleur de ne gagne pas en ligne
     }
     
-    public colonneGagnantePourCouleur (String couleur ) {
+    public boolean colonneGagnantePourCouleur (String couleur ) {
+       
+        boolean temp2 = false;
+        for (int i = 0; i < 7 ;i++){
+            for (int a = 0; a <= 2; a++){
+                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a+1,i) == couleur & lireCouleurDuJeton(a+2, i) == couleur & lireCouleurDuJeton(a+3, i) == couleur) {
+                    temp2 = true ;
+                    break;
+                }
+            }   
+        }
+    return temp2;
+    }       
         
 
             
-    }
+
     
     public diagonaleMontanteGagnantePourCouleur(String couleur) {
             
