dd29221e582ecf8794e3ad86fb27ebcb61a7718c
Thomas Maugeais <thomas.maugeais@epfedu.fr>
2022-11-30 16:16:46 +0100

    Création méthode couleur gagnante regroupant toutes les autres sous méthodes + commentaires

--- src/sp4_gouin_maugeais/PlateauDeJeu.java
+++ src/sp4_gouin_maugeais/PlateauDeJeu.java
@@ -56,76 +56,88 @@ public class PlateauDeJeu { // Création classe Plateau de jeu
     }
     
     public boolean ligneGagnantePourCouleur (String couleur) { // Cette (sous) méthode indique si une couleur que l'on donne est gagnante sur une ligne de notre plateau de jeu ou non 
-        boolean temp1 = false;
-        for (int a = 0; a < 6 ;a++){
-            if (temp1 == true){
+        boolean temp1 = false;//Variable retournée
+        for (int a = 0; a < 6 ;a++){ // on teste pour chaque ligne
+            if (temp1 == true){ // si jamais une colonne a été déttectée comme gagnante on sorte de la 1ère boucle pour return directement le résultat
                 break;
             }            
-            for (int i = 0; i <= 3; i++){
-                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a,i+1) == couleur & lireCouleurDuJeton(a, i+2) == couleur & lireCouleurDuJeton(a, i+3) == couleur) {
-                    temp1 = true ;
-                    break;
+            for (int i = 0; i <= 3; i++){ // Pour une ligne testée on teste les colonnes jusqu'à "la limite" du plateau (pas besoin de tester toutes les colonnes vu qu'on le fait déjà dans la condition ci-dessous)
+                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a,i+1) == couleur & lireCouleurDuJeton(a, i+2) == couleur & lireCouleurDuJeton(a, i+3) == couleur) { // condition avec 4 cellules cotes a cotes en ligne avec jeton de couleur identiques
+                    temp1 = true ; // alors si la condition est vérifiée on met notre variable retournée comme vraie
+                    break; // on sort de la 2ème boucle 
                 }
             }   
         }
-    return temp1; 
-    }// Retourne si il y a une ligne gagnante : TRUE la couleur gagne FALSE la couleur de ne gagne pas en ligne
+    return temp1; // on retourne si il y a une couleur gagnante en ligne
+    }
     
-    public boolean colonneGagnantePourCouleur (String couleur ) {
+    public boolean colonneGagnantePourCouleur (String couleur ) { // Méthode renvoyant si une couleur est gagnante "en colonne" sur le plateau de jeu
        
-        boolean temp2 = false;
-        for (int i = 0; i < 7 ;i++){
-            if (temp2 == true){
+        boolean temp2 = false;         
+        for (int i = 0; i < 7 ;i++){     // on teste pour chaque colonne
+            if (temp2 == true){  // si jamais une colonne a été déttectée comme gagnante on sorte de la 1ère boucle pour return directement le résultat
                 break;
             }
-            for (int a = 0; a <= 2; a++){
-                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a+1,i) == couleur & lireCouleurDuJeton(a+2, i) == couleur & lireCouleurDuJeton(a+3, i) == couleur) {
-                    temp2 = true ;
-                    break;
+            for (int a = 0; a <= 2; a++){ // Pour une colonne testée on teste les ligne jusqu'à "la limite" du plateau (pas besoin de tester toutes les lignes)
+                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a+1,i) == couleur & lireCouleurDuJeton(a+2, i) == couleur & lireCouleurDuJeton(a+3, i) == couleur) { // condition avec 4 cellules cotes a cotes en colonne avec jeton de couleur identiques
+                    temp2 = true ; // alors si la condition est vérifiée on met notre variable retournée comme vraie
+                    break; // on sort de la 2ème boucle 
                 }
             }   
         }
-    return temp2;
+    return temp2; // on retourne si il y a une couleur gagnante en colonne
     }       
         
 
             
 
     
-    public boolean diagonaleMontanteGagnantePourCouleur(String couleur) {
+    public boolean diagonaleMontanteGagnantePourCouleur(String couleur) { // Méthode renvoyant si une couleur est gagnante "en diaguonale montante" sur le plateau de jeu
        
-        boolean temp3 = false;
-        for (int i = 3; i < 7 ;i++){
-            if (temp3 == true){
+        boolean temp3 = false;// Variable retournée
+        for (int i = 3; i < 7 ;i++){//première bouvle pour les colonnes
+            if (temp3 == true){//on sort de la 1ère boucle si on a déjà detecté une couleur gagnante
                 break;
             }
-            for (int a = 0; a <= 2; a++){
-                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a+1,i-1) == couleur & lireCouleurDuJeton(a+2, i-2) == couleur & lireCouleurDuJeton(a+3, i-3) == couleur) {
-                    temp3 = true ;
-                    break;
+            for (int a = 0; a <= 2; a++){ // 2ème boucle pour les lignes
+                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a+1,i-1) == couleur & lireCouleurDuJeton(a+2, i-2) == couleur & lireCouleurDuJeton(a+3, i-3) == couleur) { // conditions victoire
+                    temp3 = true ;// si conditions de victoire vérifiées on met la variable retournée à true 
+                    break; // on sort de la 2ème boucle 
                 }
             }   
         }
-        return temp3;       
+        return temp3;   // on retourne la variable    
     }
     
-    public boolean diagonaleDescendanteGagnantePourCouleur(String couleur) {
+    public boolean diagonaleDescendanteGagnantePourCouleur(String couleur) { // Méthode renvoyant si une couleur est gagnante "en diaguonale descendante" sur le plateau de jeu
       
-        boolean temp4 = false;
-        for (int i = 0; i < 4 ;i++){
-            if (temp4 == true){
+        boolean temp4 = false; // variable retournée
+        for (int i = 0; i < 4 ;i++){ // première boucle pour les colonnes
+            if (temp4 == true){ // on sort de la 1ère boucle si on a déjà detecté une couleur gagnante
                 break;
             }
-            for (int a = 0; a <= 2; a++){
-                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a+1,i+1) == couleur & lireCouleurDuJeton(a+2, i+2) == couleur & lireCouleurDuJeton(a+3, i+3) == couleur) {
-                    temp4 = true ;
+            for (int a = 0; a <= 2; a++){ // 2ème boucle pour les lignes
+                if ( lireCouleurDuJeton(a,i) == couleur & lireCouleurDuJeton(a+1,i+1) == couleur & lireCouleurDuJeton(a+2, i+2) == couleur & lireCouleurDuJeton(a+3, i+3) == couleur) {// Conditions de diaguonale gagnante
+                    temp4 = true ; //On a une victoire donc la variable retournée est mise à true 
                     break;
                 }
             }   
         }
-        return temp4;         
+        return temp4;  // on retourne la variable        
         
     }
+    
+    public boolean GagnantePourCouleur(String CouleurTest){ // méthode réunissant toutes les sous méthodes crées précedement 
+        boolean CouleurWin = false; // variable retournée
+                
+        if (diagonaleDescendanteGagnantePourCouleur(CouleurTest)== true || diagonaleMontanteGagnantePourCouleur(CouleurTest) == true || colonneGagnantePourCouleur (CouleurTest ) == true || ligneGagnantePourCouleur (CouleurTest) == true){ //conditions si une des sous méthodes est validée 
+            
+            CouleurWin = true;// si une des sous méthodes donne une couleur gagnante alors on met la variable retournée à "true"
+        
+        }
+    return CouleurWin;   // on retourne la variable de victoire ou non  
+    }
+    
 
 }
 
